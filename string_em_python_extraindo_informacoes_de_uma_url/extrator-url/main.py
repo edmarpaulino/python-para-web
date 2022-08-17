af21f74d964feb2902aed005226055110ba2681f
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2022-08-17 18:54:45 -0300

    curso string em python extraindo informacoes de uma url finalizado

---
+++ string_em_python_extraindo_informacoes_de_uma_url/extrator-url/main.py
@@ -0,0 +1,30 @@
+# url = "https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+url = ""
+
+# Sanitização da URL
+url = url.replace(" ", "");
+
+# Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia")
+
+indice_interrogacao = url.find("?")
+
+if indice_interrogacao == -1:
+    print("Caractere '?' não foi encontrado")
+else:
+    # Separa base e os parâmetros
+    url_base = url[:indice_interrogacao]
+    url_parametros = url[indice_interrogacao + 1:]
+    print(url_parametros)
+
+    # Busca o valor de um parâmetro
+    parametro_busca = "quantidade"
+    indice_parametro = url_parametros.find(parametro_busca)
+    indice_valor = indice_parametro + len(parametro_busca) + 1
+    indice_e_comercial = url_parametros.find("&", indice_valor)
+    if indice_e_comercial == -1:
+        valor = url_parametros[indice_valor:]
+    else:
+        valor = url_parametros[indice_valor:indice_e_comercial]
+    print(valor)
